14c18efe05e54bafa5fd76927dc8a35d013478aa
caspy77 <161266204+caspy77@users.noreply.github.com>
2024-05-09 21:47:55 +0000

    I finally managed to add the image but I had to delete the basic code given by the teacher and replace it with a somewhat different one.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from tkinter import *
-import random
 
 #**********************Starting Point of program*************************#
-
 if __name__ == '__main__':
     root = Tk()
+    bg=PhotoImage(file = "first.png")
+    bglabel=Label(root,image=bg)
+    bglabel.place(x=0,y=0)
     root.title("Spacial Awareness Quiz")
     root.geometry("500x500")
     root.mainloop()
