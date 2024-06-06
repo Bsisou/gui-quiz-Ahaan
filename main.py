ecffe24ed10857e100e358b6d96920e0f92a6a73
caspy77 <161266204+caspy77@users.noreply.github.com>
2024-06-06 21:09:17 +0000

    I have now created the continue button

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 from tkinter import *
 
+#class to determine what goes on the screen
+class SpatialStart:
+    def __init__(self, start):
+        #name label
+        name_label: Label = Label(start, text="Please enter Name ")
+        name_label.place(x=200,y=300)
+
+        #name entry
+        self.name_entry = Entry(start)
+        self.name_entry.place(x=175,y=325)
+       
+#this is the button to continue to second page#
+        self.startbtn=Button(start, text="start", font=("arial", 8), background="black", foreground = "white" , command=self.Start)
+        self.startbtn.place(x=330, y=323)
+    def Start(self):
+            username=self.name_entry.get()
+            names.append(username)
+            start.destroy()
+
+
 #**********************Starting Point of program*************************#
 if __name__ == '__main__':
-    root = Tk()
+    start = Tk()
     bg=PhotoImage(file = "first.png")
-    bglabel=Label(root,image=bg)
+    bglabel=Label(start,image=bg)
     bglabel.place(x=0,y=0)
-    root.title("Spacial Awareness Quiz")
-    root.geometry("500x500")
-    root.mainloop()
+    start.title("Spacial Awareness Quiz")
+    start.geometry("500x500")
+    SpatialStart(start) #class to start the program
+    start.mainloop()
+    questions =  Tk()
